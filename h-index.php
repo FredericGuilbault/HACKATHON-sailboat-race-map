dd6f5b0caf0470fa376b14806b5a09321a289b33
Sofian Benaissa <me@sfyn.net>
2018-04-29 14:35:08 -0400

    Have a page title block

--- h-index.php
+++ h-index.php
@@ -1,8 +1,8 @@
 <html>
 <?php $pageTitle = "Accueil"; include('inc/head.inc'); ?>
 <body>
+<?php include('inc/page-title.inc'); ?>
     <div id="main-menu" class="centered narrow">
-        <h1>Accueil</h1>
         <a href="public.php"><button> Carte </button></a><br><br>
         <a href="courseur.php"><button> Courseur </button></a><br><br>
         <a href="officiels.php"><button> Officiels </button></a><br><br>
