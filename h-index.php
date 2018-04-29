c19b51e2b9e91ab729943e9ad5d98ee13b252f08
Sofian Benaissa <me@sfyn.net>
2018-04-29 14:51:09 -0400

    Push look for h-index

--- h-index.php
+++ h-index.php
@@ -1,8 +1,8 @@
 <html>
 <?php $pageTitle = "Accueil"; include('inc/head.inc'); ?>
 <body>
-<div id="main-wrapper">
 <?php include('inc/page-title.inc'); ?>
+<div id="main-wrapper">
     <div id="main-menu" class="centered narrow">
         <a href="public.php"><button> Carte </button></a><br><br>
         <a href="courseur.php"><button> Courseur </button></a><br><br>
