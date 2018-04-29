6476f1394dce18ed8b28a6db942aeed5f04f2fbd
Sofian Benaissa <me@sfyn.net>
2018-04-29 14:44:52 -0400

    Begin title block theme

--- h-index.php
+++ h-index.php
@@ -1,11 +1,14 @@
 <html>
 <?php $pageTitle = "Accueil"; include('inc/head.inc'); ?>
 <body>
+<div id="main-wrapper">
 <?php include('inc/page-title.inc'); ?>
     <div id="main-menu" class="centered narrow">
         <a href="public.php"><button> Carte </button></a><br><br>
         <a href="courseur.php"><button> Courseur </button></a><br><br>
         <a href="officiels.php"><button> Officiels </button></a><br><br>
     </div>
+</div>
 </body>
+
 </html>
